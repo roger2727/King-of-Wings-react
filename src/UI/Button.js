da4296f80362754aa65286c7cf57973aee4e6d31
roger <roger_27@live.com.au>
2022-05-25 09:58:34 +1000

    Update Button.js

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -1,6 +1,5 @@
 import React, { state } from "react";
 import styles from "../UI/Button.module.css";
-import StartGame from "../Components/StartGame.js";
 
 const clickHandler = () => {
   console.log("clickHandler");
