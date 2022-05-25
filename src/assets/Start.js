8c8efdb828ca7f038234fffa3f57161cbb3b9a57
roger <roger_27@live.com.au>
2022-05-25 10:14:02 +1000

    Delete src/assets directory

--- src/assets/Start.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import pic from "../assets/logo.png";
-import styles from "../assets/Start.module.css";
-
-function Banner() {
-  return <img className={styles.hero} src={pic} />;
-}
-
-export default Banner;
